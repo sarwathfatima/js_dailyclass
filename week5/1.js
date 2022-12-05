cfbfbb93d4c0b3328f15dbcc557b39cb0971fc06
sarwathfatima <syedasfqadri98@gmail.com>
2022-12-05 17:39:04 +0530

    this

---
+++ week5/1.js
@@ -0,0 +1,59 @@
+
+//==,===,>,<,<=,<=,!==,!=,
+//Q:-FIND THE SUM OF 'n' NATURAL NUMBERS BY WHILE LOOP:
+ let n = 1;
+ let range=10;
+//sum of no.
+ let sum = 0;
+ while(n <=range){
+     sum = sum + n;
+     n++
+ }
+  console.log(sum);//then the output will be "55"
+
+//lets do some more examples of 'n'natural numbers:
+//EXAMPLE1:Lets take n=3 and range=30:
+//  let n = 3;
+//   let range=30;
+//   //sum of no.
+//   let sum = 0;
+//   while(n <=range){
+//           sum = sum + n;
+//       n++
+//   }
+//   console.log(sum);//then the output will be "462"
+
+//EXAMPLE2:Lets take n=5 and range=5:
+//  let n = 5;
+//  let range=5;
+// //sum of no.
+//  let sum = 0;
+//  while(n <=range){
+//      sum = sum + n;
+//      n++
+//  }
+//  console.log(sum);//then the output will be "5"
+
+//EXAMPLE3:Lets take n=5 and 10:
+//  let n = 5;
+//  let range=10;
+// //sum of no.
+//  let sum = 0;
+// while(n <=range){
+//  sum = sum + n;
+//       n++
+//   }
+//   console.log(sum);//then the output will be "45"
+
+//EXAMPLE4:lets take n=7 and range=40:-
+//  let n = 7;
+// let range=40;
+// //sum of no.
+// let sum = 0;
+//  while(n <=range){
+// sum = sum + n;
+//      n++
+// }
+// console.log(sum);//then the output will be "799"
+
+  
